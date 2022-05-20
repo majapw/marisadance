9d0945c603a4246dde402817792fa97a1a55aed1
Maja Wichrowska <maja.wichrowska@airtable.com>
2022-05-20 09:22:23 -0400

    First commit I guess

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome to <code style={{display:'inline'}}>marisa.dance</code>!
+      </header>
+      <div>
+        See the many forms of Marisa Silva: 
+        <button type="button">Show Me Another</button>
+        
+      </div> 
+
+    </div>
+  );
+}
+
+export default App;
